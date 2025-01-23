e0b5fa62af4f3a89d4ad7cb3a54f3caafa3a8170
JoyceQiao7 <joyce_qiao@berkeley.edu>
2025-01-22 19:44:24 -0800

    add resume2mbti.py

---
+++ resume2mbti.py
@@ -0,0 +1,49 @@
+from openai import OpenAI
+import requests
+import base64
+
+def image_2_question(image_path):
+    # OpenAI API Key
+    api_key = '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' # 在此输入API密钥
+    # Function to encode the image
+    def encode_image(path):
+        with open(path, "rb") as image_file:
+            return base64.b64encode(image_file.read()).decode('utf-8')
+    # Path to your image
+    path = image_path
+    # Getting the base64 string
+    base64_image = encode_image(path)
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {api_key}"
+    }
+    payload = {
+        "model": "gpt-4o-2024-11-20",
+        "messages": [
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        # 在此输入问题：
+                        "text": "Try to tell this person's MBTI from her resume. First give out the four letter MBTI, and then explain why do you choose those four letters."
+                                "Here are some advice on how to make a more accurate estimation:"
+                                "1) If personal interest is listed out in the resume, depend at least half of your reasoning on that."
+                                "2) Do not reason only from what specific things that person did, but also look into the style of her writing. For example, does she exaggerate a lot or is more down-to-earth."
+                                "3) It's ok to use law of large numbers. For example, programmers are usually shy, thus might be introverted (I)."
+                    },
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": f"data:image/jpeg;base64,{base64_image}"
+                        }
+                    }
+                ]
+            }
+        ],
+        "max_tokens": 300
+    }
+    response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+    return response.json()['choices'][0]['message']['content']
+# 测试
+print(image_2_question("Joyce.png"))
